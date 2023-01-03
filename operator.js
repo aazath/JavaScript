b99825df6ab68f24588d1c385bb9d3ca278722c4
aazath <asaazath@gmail.com>
2023-01-03 15:50:29 +0530

    Operator programme

---
+++ operator.js
@@ -0,0 +1,10 @@
+// var num1 = 7;
+// var num2 = 8;
+
+// console.log(num1 + num2);
+
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent = ((listingPrice - sellingPrice) /listingPrice) * 100;
+console.log("Discount Percentage is :" + discountPercent);
